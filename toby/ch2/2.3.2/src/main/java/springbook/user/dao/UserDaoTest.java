d6b67fbb43b99ad9cdffa07b0b05a17aa528e1b8
genius.choi <genius.choi@kakaocorp.com>
2017-07-20 11:29:49 +0900

    2.3.2

---
+++ toby/ch2/2.3.2/src/main/java/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,42 @@
+package springbook.user.dao;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class UserDaoTest {
+
+	@Test
+	public void addAndGet() throws SQLException {
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
+
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+
+		User user = new User();
+		user.setId("genius");
+		user.setName("최성조");
+		user.setPassword("1234");
+
+		dao.add(user);
+
+		assertThat(dao.getCount(), is(1));
+
+		User user2 = dao.get(user.getId());
+
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+
+	public static void main(String[] args) {
+		JUnitCore.main("springbook.user.dao.UserDaoTest");
+	}
+}
