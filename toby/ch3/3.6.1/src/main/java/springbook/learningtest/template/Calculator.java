1ced681c629e538f9b923e19912802de5eea53bd
genius.choi <genius.choi@kakaocorp.com>
2017-07-26 14:28:19 +0900

    콜백/템플릿 연습

---
+++ toby/ch3/3.6.1/src/main/java/springbook/learningtest/template/Calculator.java
@@ -0,0 +1,54 @@
+package springbook.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+
+	public Integer calcSum(String filePath) throws IOException {
+		return fileReadTemplate(filePath, (BufferedReader br) -> {
+					Integer sum = 0;
+					String line;
+					while ((line = br.readLine()) != null) {
+						sum += Integer.valueOf(line);
+					}
+					return sum;
+				}
+		);
+	}
+
+	public Integer calcMultiply(String filePath) throws IOException {
+		return lineReadTemplate(filePath, (String line, Integer res) -> res *= Integer.valueOf(line), 1);
+	}
+
+	public String concatenate(String filePath) throws IOException {
+		return lineReadTemplate(filePath, (String line, String res) -> res + line, "");
+	}
+
+	private Integer fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			int ret = callback.doSomethingWithReader(br);
+			br.close();
+			return ret;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+	}
+
+	private <T> T lineReadTemplate(String filePath, LineCallback<T> callback, T initVal) throws IOException {
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+			T res = initVal;
+			String line;
+			while ((line = br.readLine()) != null) {
+				res = callback.doSomethingWithLine(line, res);
+			}
+			br.close();
+			return res;
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		}
+	}
+}
